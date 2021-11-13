528295771e8e74d1f7240fb58b696dcc74c1f87a
Tolga Boztuna <tolga.boztuna@freebirdairlines.com>
2021-11-14 01:35:47 +0300

    add AuthorizesRequests trait to Controller

--- src/Units/Controller.php
+++ src/Units/Controller.php
@@ -5,12 +5,14 @@ namespace Lucid\Units;
 use Lucid\Bus\ServesFeatures;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 /**
  * Base controller.
  */
 class Controller extends BaseController
 {
+    use AuthorizesRequests;
     use ValidatesRequests;
     use ServesFeatures;
 }
